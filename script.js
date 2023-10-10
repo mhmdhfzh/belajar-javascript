24329e9dedfefcd7145709af33e39fdf26cd8857
mhmdhfzh <muhamadhafizhsantana3233@gmail.com>
2023-10-10 09:13:13 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var x = 5;
+var y = 15;
+
+
+
+document.write("x = ", x, "<br>")
+document.write("y = ", y, "<br>")
+var hasilPertambahan = x + y;
+document.write("Hasil Pertambahan: " + hasilPertambahan, "<br>");
+
+
+var hasilPengurangan = x - y;
+document.write("Hasil Pengurangan: " + hasilPengurangan,"<br>" );
+
+
+var hasilPerkalian = x * y;
+document.write("Hasil Perkalian: " + hasilPerkalian,"<br>");
+
+
+var hasilPembagian = x / y;
+document.write("Hasil Pembagian: " + hasilPembagian,"<br>");
+
+
+var hasilModulus = x % y;
+document.write("Hasil Modulus: " + hasilModulus,"<br>");
